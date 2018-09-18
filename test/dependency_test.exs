e2781b391f3993680b49a7a439a37add109a4988
Josh Nussbaum <joshnuss@gmail.com>
2018-09-17 21:46:47 -0400

    Adds tests

--- test/dependency_test.exs
+++ test/dependency_test.exs
@@ -1,4 +1,23 @@
 defmodule DependencyTest do
   use ExUnit.Case
   doctest Dependency
+
+  test "raises MissingError when dependency was not defined" do
+    assert_raise Dependency.MissingError, fn ->
+      Dependency.resolve(Foo)
+    end
+  end
+
+  test "resolves with registered dependency" do
+    Dependency.register(Foo, Bar)
+
+    assert Bar == Dependency.resolve(Foo)
+  end
+
+  test "register replaces last value" do
+    Dependency.register(Foo, Bar)
+    Dependency.register(Foo, Baz)
+
+    assert Baz == Dependency.resolve(Foo)
+  end
 end
